3c149d255514478e055cc0bd2feec35ced26bd1a
Xavier Shay <contact@xaviershay.com>
2022-12-20 08:41:25 +1100

    Notes about dependencies

---
+++ README.md
@@ -0,0 +1,15 @@
+## Dependencies
+
+### Ruby
+
+Version 3+ with bundler.
+
+### Pandoc
+
+As of 2022-12-20, apt has an older version of pandoc that doesn't support
+templates. You'll need to install it from https://pandoc.org/installing.html
+Current development is done with 2.19.2.
+
+### Chromedriver
+
+Install from https://chromedriver.chromium.org/
--- spec/home_spec.rb
+++ spec/home_spec.rb
@@ -5,4 +5,19 @@ feature 'home page' do
     visit '/'
     expect(page).to have_content(/Xavier Shay/i)
   end
+
+  it 'shows an index of all posts' do
+    visit '/'
+    click_link 'A System for Email'
+    expect(page).to have_content("A System for Email")
+    expect(page).to have_content("you need to be able to move fast")
+  end
+
+  it 'has posts at consistent urls' do
+    visit '/articles/a-system-for-email.html'
+    expect(page).to have_content("A System for Email")
+    expect(page).to have_content("you need to be able to move fast")
+  end
+
+  it 'segments posts by year'
 end
