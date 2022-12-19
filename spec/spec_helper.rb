3c8d31cf409d11c5ce929f4d68edb73e26aa66b7
Xavier Shay <contact@xaviershay.com>
2022-12-20 08:47:59 +1100

    Tidying

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,5 @@ Capybara.configure do |c|
   c.run_server = false
   c.default_driver = ENV.fetch("DRIVER", "selenium_chrome_headless").to_sym
   c.app_host = ENV.fetch("APP_HOST", "https://blog.xaviershay.com")
+  c.default_max_wait_time = 1
 end
