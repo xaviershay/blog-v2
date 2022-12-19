27ac67488ae35e67d54273d45c6beea5e6b42e95
Xavier Shay <contact@xaviershay.com>
2022-12-20 08:47:54 +1100

    Serve index page at /

--- src/ruby/server.rb
+++ src/ruby/server.rb
@@ -1,55 +1,36 @@
 require 'webrick'
 require 'set'
 
-MIME_TYPES = {
-  "html" => "text/html"
-}
 prefix = "out/site"
-KNOWN_FILES = Set.new(Dir["#{prefix}/**/*"].select {|x| File.file?(x) }.map {|x| x[prefix.length..-1] })
-puts KNOWN_FILES
+KNOWN_FILES = Set
+  .new(Dir["#{prefix}/**/*"]
+  .select {|x| File.file?(x) }
+  .map {|x| x[prefix.length..-1] })
 
-def mime_type_for(ext)
-  MIME_TYPES.fetch(ext, "text/plain")
-end
-
-# class Server < WEBrick::HTTPServlet::AbstractServlet
-#   def do_GET (request, response)
-#     ext = File.extname(request.path)[1..-1]
-# 
-#     if KNOWN_FILES.include?(request.path)
-#       path = "out/site#{request.path}"
-#       content = File.read(path)
-#       response.status = 200
-#       puts ext
-#       response['Content-Type'] = mime_type_for(ext)
-#       response.body = 'Hello, World! ' + request.path
-#     else
-#       response.status = 404
-#       response.body = 'Not Found'
-#     end
-#   end
-# 
-#   def do_POST (request, response)
-#     puts "this is a post request who received #{request.body}"
-#   end
-# end
 GZIP_EXTS = Set.new(%w(.html))
 
+# Some content types are gzip'd on disk, since that's the format we need to
+# upload to AWS and have them serve correctly. This servlet "knows" which
+# formats are gzip'd and sets content encoding header appropriately.
 class Server < WEBrick::HTTPServlet::DefaultFileHandler
   def do_GET(req, res)
     super(req, res)
     ext = File.extname(req.path)
-    res['Content-Encoding'] = 'gzip' if GZIP_EXTS.include?(ext)
+
+    if GZIP_EXTS.include?(ext) || ext.empty?
+      res['Content-Encoding'] = 'gzip'
+    end
   end
 end
 server = WEBrick::HTTPServer.new(:Port => 4001)
 
+server.mount '/', Server, File.join(prefix, 'index.html')
 KNOWN_FILES.each do |file|
   server.mount file, Server, File.join(prefix, file)
 end
 
 trap("INT") {
-    server.shutdown
+  server.shutdown
 }
 
 server.start
