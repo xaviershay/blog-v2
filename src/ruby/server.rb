024431481844b47d3e67767c5f7e27db7a891445
Xavier Shay <contact@xaviershay.com>
2022-12-22 14:46:23 +1100

    Quick hack to serve 404s properly

--- src/ruby/server.rb
+++ src/ruby/server.rb
@@ -1,11 +1,11 @@
 require 'webrick'
 require 'set'
 
-prefix = "out/site"
+PREFIX = "out/site"
 KNOWN_FILES = Set
-  .new(Dir["#{prefix}/**/*"]
+  .new(Dir["#{PREFIX}/**/*"]
   .select {|x| File.file?(x) }
-  .map {|x| x[prefix.length..-1] })
+  .map {|x| x[PREFIX.length..-1] })
 
 GZIP_EXTS = Set.new(%w(.html))
 
@@ -14,19 +14,27 @@ GZIP_EXTS = Set.new(%w(.html))
 # formats are gzip'd and sets content encoding header appropriately.
 class Server < WEBrick::HTTPServlet::DefaultFileHandler
   def do_GET(req, res)
-    super(req, res)
-    ext = File.extname(req.path)
+    to_match = @local_path[PREFIX.length .. -1]
 
-    if GZIP_EXTS.include?(ext) || ext.empty?
-      res['Content-Encoding'] = 'gzip'
+    if req.path == to_match || req.path == "/" && to_match == "/index.html"
+      super(req, res)
+      ext = File.extname(req.path)
+
+      if GZIP_EXTS.include?(ext) || ext.empty?
+        res['Content-Encoding'] = 'gzip'
+      end
+    else
+      res.status = 404
+      res['Content-Type'] = 'text/plain'
+      res.body = "Not Found"
     end
   end
 end
 server = WEBrick::HTTPServer.new(:Port => 4001)
 
-server.mount '/', Server, File.join(prefix, 'index.html')
+server.mount '/', Server, File.join(PREFIX, 'index.html')
 KNOWN_FILES.each do |file|
-  server.mount file, Server, File.join(prefix, file)
+  server.mount file, Server, File.join(PREFIX, file)
 end
 
 trap("INT") {
