608689b018dd6d2a970087d74989ca1e2d7ff400
Xavier Shay <contact@xaviershay.com>
2023-01-07 14:59:06 +1100

    Genre filtering

--- src/static/js/book-filter.js
+++ src/static/js/book-filter.js
@@ -1,9 +1,29 @@
-document.getElementById('book-filter').addEventListener('change', function() {
-  var ratingElements = document.querySelectorAll('.book-summary');
-  for (var i = 0; i < ratingElements.length; i++) {
-    var rating = ratingElements[i].getAttribute('data-rating');
-    if (rating < 4) {
-      ratingElements[i].style.display = this.checked ? 'none' : 'block';
+var bookFilter = document.getElementById('book-filter');
+var genre = null;
+
+function filterElements() {
+  var elements = document.querySelectorAll('.book-summary');
+  elements.forEach(element => {
+    matchingGenre = !genre || element.classList.contains(genre);
+    matchingRating = !bookFilter.checked || element.getAttribute('data-rating') >= 4
+    element.style.display = matchingGenre && matchingRating ? 'block' : 'none';
+  });
+}
+
+bookFilter.addEventListener('change', function() {
+  filterElements();
+});
+
+var genreFilters = document.querySelectorAll('.genre-filter')
+genreFilters.forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    var newGenre = this.getAttribute('data-genre')
+    if (newGenre == genre) {
+      genre = null;
+    } else {
+      genre = newGenre;
     }
-  }
+    filterElements();
+  });
 });
