dd80cf2048c18cbb75844185dd161b4807adc550
Xavier Shay <contact@xaviershay.com>
2024-12-26 12:02:54 +1100

    2024 reading list and draft of review

--- misc/process_gratitude.rb
+++ misc/process_gratitude.rb
@@ -1,13 +1,22 @@
 require 'json'
 require 'pp'
+require 'date'
 
-data = JSON.parse(File.read('tmp/gratitudes-data.json'))
+cutoff = Date.new(2024,1,1)
+
+puts cutoff
+data = JSON.parse(File.read('tmp/gratitudes-data-2024.json')).fetch('entries')
   # Entry data is double JSON encoded
-  .map {|x| JSON.parse(x[1]) }
+  .map {|x| JSON.parse(x) }
+  .select {|x| Date.parse(x.fetch('date')) >= cutoff }
 
-pp data.length
+data.each do |x|
+  puts x.fetch('date')
+  puts x.fetch('entry')
+  puts
+end
 
-pp data.select {|x| x['entry'] }.filter {|x| x['entry'].lines.any? {|y| y =~ /jodie/i  } }.length
-pp data.select {|x| x['entry'] }.filter {|x| x['entry'].lines.any? {|y| y =~ /jodie/i && y =~ /crossword/i } }.length
-pp data.select {|x| x['entry'] }.filter {|x| x['entry'].lines.any? {|y| y =~ /crossword/i } }.length
-pp data.select {|x| x['entry'] }.filter {|x| x['entry'].lines.any? {|y| y =~ /pinball/i || y =~ /godzilla/i } }.length
+# pp data.select {|x| x['entry'] }.filter {|x| x['entry'].lines.any? {|y| y =~ /jodie/i  } }.length
+# pp data.select {|x| x['entry'] }.filter {|x| x['entry'].lines.any? {|y| y =~ /jodie/i && y =~ /crossword/i } }.length
+# pp data.select {|x| x['entry'] }.filter {|x| x['entry'].lines.any? {|y| y =~ /crossword/i } }.length
+# pp data.select {|x| x['entry'] }.filter {|x| x['entry'].lines.any? {|y| y =~ /pinball/i || y =~ /godzilla/i } }.length
